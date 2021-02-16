c14338af3f6ea4518fe97857468cf8a13a271441
unknown <mysocialmedia69@yahoo.com>
2021-02-16 06:11:01 +0000

    added testimonials section

--- js/index.js
+++ js/index.js
@@ -9,3 +9,32 @@ menu.addEventListener("click", function () {
 close.addEventListener("click", function () {
   sidenav.classList.remove("active");
 });
+
+// testimonials
+const texts = document.querySelectorAll(".text-unit");
+const faces = document.querySelectorAll(".face");
+
+clear();
+
+// add active to first element
+texts[0].classList.add("active");
+faces[0].classList.add("active");
+
+faces.forEach((face, index) => {
+  face.addEventListener("click", function () {
+    clear();
+
+    this.classList.add("active");
+
+    texts[index].classList.add("active");
+
+    console.log(texts[index]);
+  });
+});
+
+function clear() {
+  texts.forEach((text) => text.classList.remove("active"));
+  faces.forEach((face) => face.classList.remove("active"));
+
+  console.log("cleared");
+}
